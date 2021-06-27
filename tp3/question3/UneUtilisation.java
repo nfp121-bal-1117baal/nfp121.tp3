001239cbe8fcd1909972e2fc6e1d811611e6537f
FadlallahMedlej <fadlallah.medlej@isae.edu.lb>
2021-06-27 22:57:45 +0300

    work

--- tp3/question3/UneUtilisation.java
+++ tp3/question3/UneUtilisation.java
@@ -1,14 +1,16 @@
 package question3;
 
 import question1.PolygoneRegulier;
-
 public class UneUtilisation {
 
     public static void main(String[] args) throws Exception {
-        // déclarer p1
-        // déclarer p2
 
-        // p1 est ici une pile de polygones réguliers PolygoneRegulier.java
+        // d�clarer p1
+        // d�clarer p2
+        PileI<PolygoneRegulier> p1 = new Pile2<PolygoneRegulier>();
+        PileI<Object> p2 = new Pile2<Object>();
+        
+        
         p1.empiler(new PolygoneRegulier(4, 100));
         p1.empiler(new PolygoneRegulier(5, 100));
 
@@ -16,16 +18,8 @@ public class UneUtilisation {
 
         p2.empiler(p1);
         System.out.println(" la pile p2 = " + p2);
+      
 
-        try {
-            // p1.empiler(new PolygoneRegulier(5,100)); // désormais une erreur de
-            // compilation
-            // ....
-            // String s = (String)p1.depiler(); // désormais une erreur de
-            // compilation
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+
     }
-
 }
